0f37a43320d4dfef8759110af2f3cc184680fc66
sherlockian4869 <ya0813kendo@gmail.com>
2020-12-30 14:24:17 +0900

    Initial Commit

---
+++ CustomClock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomClock
+//
+//  Created by Kohei Yaeo on 2020/12/30.
+//  Copyright © 2020 sherlockian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
