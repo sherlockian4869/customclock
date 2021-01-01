11f0768588053396d861272406775793681ce1ee
sherlockian4869 <ya0813kendo@gmail.com>
2021-01-01 23:25:12 +0900

    first commit

---
+++ CustomClock/ViewControllers/SelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectViewController.swift
+//  CustomClock
+//
+//  Created by Kohei Yaeo on 2021/01/01.
+//  Copyright © 2021 sherlockian. All rights reserved.
+//
+
+import UIKit
+
+class SelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CustomClock/ViewControllers/StopWatchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StopWatchViewController.swift
+//  CustomClock
+//
+//  Created by Kohei Yaeo on 2020/12/30.
+//  Copyright © 2020 sherlockian. All rights reserved.
+//
+
+import UIKit
+
+class StopWatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
